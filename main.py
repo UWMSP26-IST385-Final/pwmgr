84e0023b8ec5f4cd07d43354e376dd8fc4821ce8
SEWsam <ponchiesam@gmail.com>
2026-05-09 00:59:38 -0500

    feat: implement basic password generation
    corrects commit 87c67dd which did not push initial content of main.py

---
+++ main.py
@@ -0,0 +1,101 @@
+"""
+ist385 final
+"""
+
+import secrets
+import string
+import math
+from itertools import combinations
+
+# TODO: PEP257 & Sphinx compliant docstrings
+# TODO: transition from TUI to GUI
+# TODO: finish password strength (entropy) checker
+
+
+CHARSETS = {
+    "uppercase": string.ascii_uppercase,
+    "lowercase": string.ascii_lowercase,
+    "special": string.punctuation,
+    "numbers": string.digits,
+}
+
+
+"""
+map every possible combination of the charsets above to the number of bits of entropy
+per character of a string using the given charset
+"""
+
+
+def make_entropy_map() -> dict[frozenset, float]:
+    ret = {}
+    # n choose r, for every r such that 0 < r <= n
+    # aka iterate thru every combination of every size selection of charsets
+    for r in range(1, len(CHARSETS)):
+        # each choice is a tuple of keys for the CHARSETS dict
+        for choice in combinations(CHARSETS, r):
+            size = sum(len(CHARSETS[n]) for n in choice)
+            ret[frozenset(choice)] = size
+    return ret
+
+
+ENTROPY: dict[frozenset, float] = make_entropy_map()
+
+
+def check_bad_pw(pw: str):
+    # TODO: implement known bad pw checking
+    pass
+
+
+def pw_entropy(charset: str, length: int) -> float:
+    # TODO: perform calculation inline?
+    charset_size = len(set(charset))
+    return math.log2(charset_size) * length
+
+
+def main():
+    length: int
+    while True:
+        try:
+            length = int(input("Enter desired password length:"))
+            if length < 14:
+                print("Password length must be 14 characters or greater.\n")
+            elif length > 64:
+                print("Password length cannot be grater than 64 characters\n")
+            else:
+                break
+        except ValueError:
+            print("Please enter a valid integer value.\n")
+
+    # TODO: allow selection of charsets to use
+    # TODO: also allow specification of # of characters from "special"
+    available_chars = (
+        CHARSETS["uppercase"]
+        + CHARSETS["lowercase"]
+        + CHARSETS["numbers"]
+        + CHARSETS["special"]
+    )
+    password: list[str]
+    while True:
+        password = []
+        # TODO: better randomness?
+        for _ in range(length):
+            password.append(secrets.choice(available_chars))
+
+        # TODO: replace hardcoded checks
+        # also this is gross and there is 100% a better way but wtv
+        if (
+            any([letter in CHARSETS["lowercase"] for letter in password])
+            and any([letter in CHARSETS["uppercase"] for letter in password])
+            and any([letter in CHARSETS["numbers"] for letter in password])
+            and any([letter in CHARSETS["special"] for letter in password])
+        ):
+            break
+
+    generated = "".join(char for char in password)
+    entropy = pw_entropy(available_chars, len(generated))
+    print(generated)
+    print(f"Password entropy (bits): {entropy}")
+
+
+if __name__ == "__main__":
+    main()
